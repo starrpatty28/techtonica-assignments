6a4f5845e5c46b8c444897605fccd3464ffd5d71
starrpatty28 <starrpatty28@gmail.com>
2019-07-29 10:25:39 -0700

    INitial Commit for Independent Daily Work

---
+++ Independent Daily Work/7_29_19Independent.js
@@ -0,0 +1,13 @@
+//Loops
+
+//ForLoops
+    // We use ForLoops when we want to execute a chunk of code a certain amount of times
+
+    // for (let I = 0; I < 10; I++)
+
+    // 1. for (let i = 0,   First we initialize the counter variable
+    // 2. i < 10;   The loop will run each time the condition is true (as long as I is less than 10, this loop will keep running
+    // 3. I++  This is stating to add 1 to the vale of the variable each time it goes around
+
+    // “For each iteration, beginning with i = 0, run this code if i is less than 10, incrementing i by 1”
+
