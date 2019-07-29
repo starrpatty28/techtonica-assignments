bf6720c35c41fa618a848fbdb8af051c37f01c0e
starrpatty28 <starrpatty28@gmail.com>
2019-07-29 14:06:25 -0700

    Code Academy ForLoops 1st Part

--- Independent Daily Work/7_29_19Independent.js
+++ Independent Daily Work/7_29_19Independent.js
@@ -156,3 +156,29 @@ for(let i = 0; i < arrAY.length; i++) {
                              console.log('Your age is less than 10');
                          }
                           console.log('You are now over 10');;
+
+
+ // ****************************************************************************
+
+ //CodeAcademy Leeson for ForLoops
+
+//  1. Now, make your own! Create a program that loops from 5 to 10 and logs each number to the console.
+
+                            for (let i = 5; i <= 10; i++) {
+                                console.log(i);
+                            }
+                            
+
+//  2.Make a for loop that loops backwards printing 3 to 0 to the console. Use the >= comparison operator in your stopping condition and the -- operator in your iteration statement.
+                            for (let counter = 3; counter >= 0; counter--){
+                                console.log(counter);
+                            }
+
+//  1. Write a for loop that iterates through our vacationSpots array using i as the iterator variable. Inside the block of the for loop, use console.log() to log each element in the vacationSpots array after the string 'I would love to visit '. For example, the first round of the loop should print 'I would love to visit Bali' to the console.                   
+
+                const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+                for (let i = 0; i < vacationSpots.length; i++){
+                console.log('I would love to visit ' + vacationSpots[i]);
+                }
+                                    
