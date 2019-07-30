196f6206c20dd1e74892ab0c27ea8f9166352767
starrpatty28 <starrpatty28@gmail.com>
2019-07-30 10:33:39 -0700

    Finished the HackerRank Challenge w/assistance

--- Independent Daily Work/7_29_19Independent.js
+++ Independent Daily Work/7_29_19Independent.js
@@ -300,3 +300,51 @@ function printVacations (arrays) {
   //another for loop and a if else statement
   
   //will go over tomorrow
+
+ 
+  
+  // ***************************************************************************
+
+  //HackerRank Challenge
+
+//Objective
+
+//In this challenge, we practice looping over the characters of string. Check out the attached tutorial for more details.
+
+//Task
+
+//Complete the vowelsAndConsonants function in the editor below. It has one parameter, a string, , consisting of lowercase English alphabetic letters (i.e., a through z). The function must do the following:
+
+// First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
+// Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
+// Input Format
+
+// Locked stub code in the editor reads string  from stdin and passes it to the function.
+
+// Output Format
+
+// First, print each vowel in  on a new line (in the same order as they appeared in ). Second, print each consonant (i.e., non-vowel) in  on a new line (in the same order as they appeared in ).
+
+// function isVowel(char){
+//     const vowels = ['a','e','i','o','u'];
+//     for(let i = 0; i<vowels.length; i++){
+//       if(char === vowels[i]){
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+//   function vowelsAndConsonants (s){
+//     for(let i = 0; i <s.length; i++){
+//       if(isVowel(s[i])){
+//         console.log(s[i]);
+//       }
+//     }
+//     for(let i = 0; i <s.length; i++){
+//       if(!isVowel(s[i])){
+//         console.log(s[i]);
+//       }
+//     }
+//   }
+//   vowelsAndConsonants('alicia'); 
+  
