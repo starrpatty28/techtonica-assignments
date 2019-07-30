f2c9f5877d7694a726289c96791067026f450d9c
starrpatty28 <starrpatty28@gmail.com>
2019-07-30 10:58:03 -0700

    Adding my example of an if-else statement for me to make a switch case statement

---
+++ Independent Daily Work/7_30_19Independent.js
@@ -0,0 +1,22 @@
+//Switch Statements
+
+//Switch Statements: if we need to execute variious bits of code depending on whether the expressions is true or false. We can use an if-else but what is you need to have 20 if-else, that would be a lot of code. That is where the Switch statement comes in.
+
+//My example of an 'if else' statement
+
+var car = 'mercedes';
+
+if (car === 'toyota') {
+    console.log('toyota is a great car');
+} else if (car === 'chevy') {
+    console.log('chevy is the most car');
+} else if (car === 'honda') {
+    console.log('honda cars are good on gas');
+} else if (car === 'buick') {
+    console.log('buick cars have a lot of space');
+} else if (car === 'yukon') {
+    console.log('yukon cars are very pricey');
+} else {
+  console.log('You need to get a new car');
+} 
+
