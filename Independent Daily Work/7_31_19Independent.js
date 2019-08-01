2ed4e1e6952ac61c674bbbbc8cbb1f4165871a37
starrpatty28 <starrpatty28@gmail.com>
2019-07-31 23:20:20 -0700

    Completed Challenge Question 3-8

--- Independent Daily Work/7_31_19Independent.js
+++ Independent Daily Work/7_31_19Independent.js
@@ -39,10 +39,11 @@
                     }
                  };
                 sum('hello', 'world');
-                sum(5,10;)
+                sum(5,10
 
 
 // [Challenge 1] - Convert Inches to Meters
+
 // Define a function called metersToInches that takes an input of the number of meters and outputs the result of the number of inches.
 
 // Hint: 1 meter equals 39.3701 inches.
@@ -80,6 +81,7 @@ function metersToInches (num) {
 //************************************************************************
 
 // [Challenge 2] - Loop n Times
+
 // Write a function named loopThrough that takes an argument that is a number and loops through and displays a message “Hello World” that number of times.
 
 //The function takes in an number
@@ -127,6 +129,126 @@ function metersToInches (num) {
 //each time the loop goes through it has a naumber and add's each time
                 
                 
+// [Challenge 4] - FizzBuzz
 
+// Write a function called fizzbuzz that prints each number from 1 to the input on a new line.
+
+// For each multiple of 3, print "Fizz" instead of the number.
+// For each multiple of 5, print "Buzz" instead of the number.
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+
+function fizzBuzz (num) { 
+
+        if (num === 0) {
+          console.log ();
+        }
+  
+        for(let i = 1; i <= num; i++) { 
+
+         if (i % 5 === 0 && i % 3 === 0) {
+
+            console.log("FizzBuzz");
+          } else if (i % 5 === 0) {
+            console.log("Buzz");
+          } else if (i % 3 === 0) {
+            console.log("Fizz");
+          } 
+          else { 
+            console.log(i);
+          }
+        }
+      } 
+      fizzBuzz(0);
+  
+    // //check first if there is a number, if it not, it will not go through the function and return nothing
+    // //Create a for Loop to check against the number
+    // //Do you the FizzBuzz methods
+    // //You want to start with FizzBuzz conditional first to because if you dont it will hit the first two first and retuen back
+    // //no RETURN, console.log=print
+   
+    
+// [Challenge 5] Calculate Factorial Number
+
+// A factorial number is the concept in math by value of multiplying numbers by each consecutively smaller number.
+
+// For example, the value of 3 factorial, is 6, which can be calculated by:
+
+  
+function calculateFactorial(num) {
+  var results = 1;
+  for (let i = 1; i <= num; i++) {
+    results *= i;
+  }
+  return results;
+}
+calculateFactorial(3); 
+
+
+//Honestly I used the same code from Num Sum challenge and changed the operator to multiply
+// Make sure you change the operating symbol and yout start numbers should not start at 0 because any number times (0) will always be 0
+
+
+
+// [Challenge 6] Switch Statements
+
+// Using a switch statement, write a function called sleep_in(weekday, vacation) with these criteria: The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+function sleep_in(weekday, vacation) {
+
+    switch(!weekday || vacation) {
+      case true:
+      return true;
+      break;
+
+      default:
+      return false;
+      
+    }
+}
+sleep_in(false, false); // true no weekday, no vacation
+sleep_in(true, false); // false a weekday, no vacation
+sleep_in(false, true); // trueno weekday, yes vacation
+
+
+//if its no weekday(weekend) or vacation = its true sleep in
+//else false either way
+
+// Challenge 7] Accessing Arrays
+
+// Write a function called common(a, b) with these criteria: Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+function common(a,b) {
+    if( a[0] === b[0] || a[a.length-1] === b[b.length-1]) {
+    return true
+      } else {
+        return false;
+      }
+    } 
+    common([1, 2, 3], [7, 3]); 
+
+//function takes in 2 integers
+//functions spits out true or false
+
+//Game Plan
+//I first thought that I would enter in a ForLoop
+//I was told if Im checking for the first or last number, I do not need a ForLoop
+//Create an if statement to check a&b index[0]
+  //also check a&b length '-1' to get the last index
+//if either is true, then return true 
+//if not return false
+
+
+
+// [Challenge 9] Object Keys and Values
+
+// Create an object with two key-value pairs.
+
+// Log that object to the console.
+
+// Delete the first key-value pair in the object.
+
+// Log that object to the console again. The first key-value pair should be gone.
 
 
