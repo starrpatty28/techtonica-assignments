5575771e284c4a7597f8b051beba52ca8afea3a0
starrpatty28 <starrpatty28@gmail.com>
2019-08-01 14:17:15 -0700

    INitial commit for 8.1.19 Independent Work

---
+++ Independent Daily Work/8_1_19Independent.js
@@ -0,0 +1,10 @@
+// Independent Practie
+
+// Runtime Complexity Exercise 1: Reading code and analyzing runtime
+
+// Read the functions in runtime1-analyzing.js. For each function, figure out:
+
+// What does the function do?
+// What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
+// Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
+// When the input size doubles, what would happen to the time it takes to run?
