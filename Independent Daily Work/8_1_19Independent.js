ba507d9a1883b4ed2de7cdb8934a024572e85e01
starrpatty28 <starrpatty28@gmail.com>
2019-08-01 15:19:37 -0700

    Completed function #1 in Exercise #1

--- Independent Daily Work/8_1_19Independent.js
+++ Independent Daily Work/8_1_19Independent.js
@@ -1,4 +1,4 @@
-// Independent Practie
+// Independent Practice
 
 // Runtime Complexity Exercise 1: Reading code and analyzing runtime
 
@@ -8,3 +8,38 @@
 // What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
 // Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
 // When the input size doubles, what would happen to the time it takes to run?
+
+
+const execTime = (arg, functionName)  => {
+    console.time('execution time: ');
+    functionName(arg);
+    console.timeEnd('execution time: ');
+    return functionName(arg);
+} 
+
+// Problem #1
+
+const mysteryFunction0 = function(array) {
+    const myFavoriteNum = 7;
+    for (i = 0; i < array.length; i++) {
+      if (array[i] === myFavoriteNum) {
+        return true;
+      }
+    }
+    return false;
+  }
+  execTime([2,4,6,8,10,11,7],mysteryFunction0);
+
+//What does the function do?
+    the function is checking for if the array has the favorite number, if it does it will return true, if it does not it will retuen false.
+
+// // What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
+    The input size is an array with 7 integers
+
+// // Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n
+
+    O(n)
+
+// // When the input size doubles, what would happen to the time it takes to run?
+   the execuation time got shorter if the number was in the array. If the 7 was not in the array, the time got longer.
+
