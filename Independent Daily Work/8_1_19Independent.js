d2b88611046a2fb9ad699efb7979e0cd81ac9191
starrpatty28 <starrpatty28@gmail.com>
2019-08-01 15:56:47 -0700

    Completed problem 2 from Exercise 1

--- Independent Daily Work/8_1_19Independent.js
+++ Independent Daily Work/8_1_19Independent.js
@@ -43,3 +43,22 @@ const mysteryFunction0 = function(array) {
 // // When the input size doubles, what would happen to the time it takes to run?
    the execuation time got shorter if the number was in the array. If the 7 was not in the array, the time got longer.
 
+
+   // Problem #2
+
+   const mysteryFunction1 = function(array) {
+    index = 4;
+    return array[index];
+  }
+  execTime([2,4,6,8,2,3,],mysteryFunction1);
+
+// What does the function do?
+    the function is looking for index 4
+// What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
+    An array with 7 integers
+// Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
+
+    O(1)
+// When the input size doubles, what would happen to the time it takes to run?
+
+    It does not matter because index 4 will always be there
