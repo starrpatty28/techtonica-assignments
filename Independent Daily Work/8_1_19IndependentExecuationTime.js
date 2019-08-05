5ebc4b4406f39e38703d62bc58f4851d33571f45
starrpatty28 <starrpatty28@gmail.com>
2019-08-04 19:15:50 -0700

    Completed the first 4 exercises running the Big O graphs

--- Independent Daily Work/8_1_19IndependentExecuationTime.js
+++ Independent Daily Work/8_1_19IndependentExecuationTime.js
@@ -62,3 +62,79 @@ const mysteryFunction0 = function(array) {
 // When the input size doubles, what would happen to the time it takes to run?
 
     It does not matter because index 4 will always be there
+
+// Problem #3
+
+var mysteryFunction2 = function(n) {
+    let primes = []
+    for (i = 2; i < n; i++) {
+      let factorFound = false;
+      for (j = 2; j < n; j++) {
+        if (i % j == 0) {
+          factorFound = true;
+        }
+      }
+      if (factorFound === false) {
+        primes.push(i);
+      }
+    }
+    return primes.length;
+  }
+ 
+//Read the following functions. For each one, figure out:
+//What does the function do
+
+No matter what is being passed into the function, the number always comes back as 0, I beleive this function is wrong.
+
+//Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
+
+    I beleive it was an O(n^2) because of the nested loop
+
+//Run the function with a few different input sizes and see how long it takes
+  The function is not working
+//When the input size doubles, what happens to the time it takes to run?
+  The function is not working, I will have to ask about this function.
+
+  // Problem #4
+
+  const mysteryFunction3 = function(array) {
+    myLength = array.length;
+    if (myLength % 2 == 0) {
+      return "even length";
+    } else {
+      return "odd length";
+    }
+  }
+  
+//What does the function do?
+  The function takes in an array and if the array length is divisable by 2 with no remainder, it returns true, else return false
+//What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
+  The input size is an array with 6 integers inside. 
+
+
+//Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
+  I say its an O(1) and it was correct
+
+//When the input size doubles, what would happen to the time it takes to run?
+execution time: : 0.483ms
+after double execution time: : 0.502ms
+
+// Problem #5
+
+var mysteryFunction4 = function(string) {
+    let eCount = 0;
+    for (i = 0; i < string.length; i++) {
+      if (string[i] === 'e') {
+        eCount++;
+      }
+    }
+    return eCount;
+  }
+
+//What does the function do?
+
+//What is the input size? Examples include the size of a list, the length of a string, or the integer passed into a function. This will be "n" in Big O notation.
+
+//Try to figure out the runtime -- O(1), O(log n), O(n), O(n log n), O(n^2), or O(2^n)
+
+//When the input size doubles, what would happen to the time it takes to run?
