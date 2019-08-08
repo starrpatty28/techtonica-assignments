6e7061f88d3a6ac8a02a0e95aa5f8db22d002986
starrpatty28 <starrpatty28@gmail.com>
2019-08-08 10:17:36 -0700

    Created an empty array and pushed all 3 objects into that array

--- Independent Daily Work/08_08_19MiniProject/event.js
+++ Independent Daily Work/08_08_19MiniProject/event.js
@@ -6,8 +6,15 @@ class Event {
     }
   }
 
+  //Creating objects from the above class
   const event_obj1 = new Event("KLOS Golden Gala", "An evening with hollywood vampires")
   ;
   const event_obj2 = new Event("Skillet & Sevendust", "Victorious war tour");
-  
+
   const event_obj3 = new Event("Jenny Lewis", "On the line tour 2019");
+
+  //Create an empty array
+  const event_array = new Array();
+
+ //pushing multiple objects to an array at once
+event_array.push(event_obj1, event_obj2, event_obj3);
