1ebfbe5e82e44df647eadeb80c3649a7e9af8083
starrpatty28 <starrpatty28@gmail.com>
2019-08-08 13:12:45 -0700

    Add tickets and prices to event_obj2

--- Independent Daily Work/08_08_19MiniProject/event.js
+++ Independent Daily Work/08_08_19MiniProject/event.js
@@ -4,7 +4,7 @@ class Event {
       this.description = description;
       this.availableTickets = []
     }
-    //function to push to array
+    //Add a function to Event class and push to 'available ticket array
     addAvailableTickets (typ, price) {
        const ticket1 = new Ticket(typ, price);
        this.availableTickets.push(ticket1); 
@@ -37,6 +37,16 @@ event_array.push(event_obj1, event_obj2, event_obj3);
 event_obj1.addAvailableTickets("human", 299);
 event_obj1.addAvailableTickets("vampire", 99);
 console.log(event_obj1);
+
+event_obj2.addAvailableTickets("General Admission", 25)
+event_obj2.addAvailableTickets("Floor Seating", 80)
+
+event_obj2.addAvailableTickets("Orchestra", 300)
+event_obj2.addAvailableTickets("Mezzanine", 200)
+event_obj2.addAvailableTickets("Balcony", 100)
+console.log(event_obj2);
+
+
 
 $(document).ready(function() {
     let html = "";
