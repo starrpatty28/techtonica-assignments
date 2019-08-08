d72a50f864f27569b6a9feb593ece8369ebc0582
starrpatty28 <starrpatty28@gmail.com>
2019-08-08 10:15:00 -0700

    Created 3 objects and assigned values to them

--- Independent Daily Work/08_08_19MiniProject/event.js
+++ Independent Daily Work/08_08_19MiniProject/event.js
@@ -5,3 +5,9 @@ class Event {
       this.availableTickets = []
     }
   }
+
+  const event_obj1 = new Event("KLOS Golden Gala", "An evening with hollywood vampires")
+  ;
+  const event_obj2 = new Event("Skillet & Sevendust", "Victorious war tour");
+  
+  const event_obj3 = new Event("Jenny Lewis", "On the line tour 2019");
