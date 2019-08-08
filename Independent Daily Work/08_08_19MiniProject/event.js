2090c6dfa034c5efaff9ced329954769f41268a6
starrpatty28 <starrpatty28@gmail.com>
2019-08-08 13:05:55 -0700

    Add a function on the class called addAvailableTickets to create a ticket type, that will accept two parameters

--- Independent Daily Work/08_08_19MiniProject/event.js
+++ Independent Daily Work/08_08_19MiniProject/event.js
@@ -4,22 +4,39 @@ class Event {
       this.description = description;
       this.availableTickets = []
     }
+    //function to push to array
+    addAvailableTickets (typ, price) {
+       const ticket1 = new Ticket(typ, price);
+       this.availableTickets.push(ticket1); 
+    }
   }
 
-  //Creating objects from the above class
-  const event_obj1 = new Event("KLOS Golden Gala", "An evening with hollywood vampires")
+class Ticket {
+    constructor(type, price) {
+      this.type = type;
+      this.price = price;
+     }
+  }
+//Creating objects from the above class
+const event_obj1 = new Event("KLOS Golden Gala", "An evening with hollywood vampires")
   ;
-  const event_obj2 = new Event("Skillet & Sevendust", "Victorious war tour");
+const event_obj2 = new Event("Skillet & Sevendust", "Victorious war tour");
 
-  const event_obj3 = new Event("Jenny Lewis", "On the line tour 2019");
+const event_obj3 = new Event("Jenny Lewis", "On the line tour 2019");
 
-  //Create an empty array
-  const event_array = new Array();
+//Create an empty array
+const event_array = new Array();
 
- //pushing multiple objects to an array at once
+//pushing multiple objects to an array at once
 event_array.push(event_obj1, event_obj2, event_obj3);
 
-console.log(event_array);
+//check to see if the events are in the array
+    //console.log(event_array);
+
+
+event_obj1.addAvailableTickets("human", 299);
+event_obj1.addAvailableTickets("vampire", 99);
+console.log(event_obj1);
 
 $(document).ready(function() {
     let html = "";
@@ -34,3 +51,12 @@ $(document).ready(function() {
 
 // .html() is used to return the HTML code from jQuery to the target element of the HTML page.
 // event is the target element in the below code.
+
+
+//create another class, "Ticket" with two properties on it, and within your function definition, you will need to modify your availableTickets property.
+
+
+
+// Now that you have the basic class working, let's add a useful function to your class. Add a function on the class called addAvailableTickets to create a ticket type, that will accept two parameters. The first parameter will be the ticket type, and the second parameter will be the price of the ticket. The method should look like this when it is called:
+
+// Hint: You might want to create another class, "Ticket" with two properties on it, and within your function definition, you will need to modify your availableTickets property.
