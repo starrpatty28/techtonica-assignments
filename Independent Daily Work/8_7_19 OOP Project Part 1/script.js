80d3231e8c7b34a900030b08bd84112b9915a787
starrpatty28 <starrpatty28@gmail.com>
2019-08-09 14:20:01 -0700

    Added personalized events to users

--- Independent Daily Work/8_7_19 OOP Project Part 1/script.js
+++ Independent Daily Work/8_7_19 OOP Project Part 1/script.js
@@ -1,8 +1,12 @@
 class Users {
     constructor(name) {
      
-    this.name = name;    
+    this.name = name;
+    this.userEvents = [];   
     }
+    addFavoriteEvents(chosenEvent) {
+        this.userEvents.push(chosenEvent);
+     }
 }
 
 class Events {
@@ -27,50 +31,75 @@ class EventRecommender {
     }
     
     addUser(new_users) {
+    // Adds a new Event to the System
         this.users.push(new_users);
     }
 
-    saveUserEvent(){
-    //Allow users to save events to a personal Events array.
-    }
+    // saveUserEvent(users, event){
+    // //Allow users to save events to a personal Events array.
+    // }
 
-    deleteUser() {
+    deleteUser(name) {
     // Deletes a User from the system
+    this.users = this.users.filter( users => users.name != name );
     }
    
-    deleteEvent() {
+    deleteEvent(name) {
     // Deletes the Event from the system
-    }
+    this.events = this.events.filter( events => events.name != name );
+     }
 
-    filter(){
+    filter(minPrice, maxPrice){
     }
 }
 //Adding an object for the 'Rec' class
     const event_rec = new EventRecommender();
 
 //Adding an object for the 'Users' class
+    const user_Dominique = new Users('Dominique');
+    event_rec.addUser(user_Dominique);
+
     const user_Alicia = new Users('Alicia');
     event_rec.addUser(user_Alicia);
-    console.log(event_rec);
+
+    const user_Kamiah = new Users('Kamiah');
+    event_rec.addUser(user_Kamiah);
+    //console.log(event_rec);
 
 //Adding an obeject for the 'Events' class
-    const event1 = new Events('TuPac_concert', 800);
+    const event1 = new Events('TuPac_concert', 1000);
     event_rec.addEvent(event1);
+
+    const event2 = new Events('WuTang_concert', 600);
+    event_rec.addEvent(event2);
+     
+    const event3 = new Events('Biggie_concert', 800);
+    event_rec.addEvent(event3);
+    //console.log(event_rec);
+
+
+ // Delete a User from the System
+    event_rec.deleteUser('Alicia');
+    //console.log(event_rec);
+
+
+ // Delete a Event from the System
+    event_rec.deleteEvent('WuTang_concert');
     console.log(event_rec);
 
 
-// Users:
-// Add a new User to the System
+//Save a user favorite event
+    user_Alicia.addFavoriteEvents(event1)
+    user_Dominique.addFavoriteEvents(event2);
+    user_Kamiah.addFavoriteEvents(event3);
+    console.log(user_Alicia);
+    console.log(user_Dominique);
+    console.log(user_Kamiah);
 
-// Allow users to save events to a personal Events array //make an empty aray in the users class
 
-// Delete a User from the System
 
-// Event:
-// Add a new Event to the System
-
-// Delete an Event from the System
 
 // Filter:
+
 // Note: You can use two separate filter functions or one if you'd like an extra challenge :)
 
