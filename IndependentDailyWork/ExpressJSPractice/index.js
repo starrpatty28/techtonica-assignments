f215752357d95d0e77973727c54c1ef51fcf6c07
starrpatty28 <starrpatty28@gmail.com>
2019-08-21 08:13:19 -0700

    Add code to grab a single member from the members.js file

--- IndependentDailyWork/ExpressJSPractice/index.js
+++ IndependentDailyWork/ExpressJSPractice/index.js
@@ -1,21 +1,24 @@
-const express = require ('express');
-const logger = require('./middleware/logger');
-const path = require('path')
-const members = require('./Members');
+const express = require("express");
+const logger = require("./middleware/logger");
+const path = require("path");
+const members = require("./Members");
 
 const app = express();
-
 
 // //Initialize the MiddleWare
 // app.use(logger);
 
 //Gets All Members
-app.get('/api/members', (req, res) => res.json(members));
+app.get("/api/members", (req, res) => res.json(members));
+
+//Get Single Member
+app.get("/api/members/:id", (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+});
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
