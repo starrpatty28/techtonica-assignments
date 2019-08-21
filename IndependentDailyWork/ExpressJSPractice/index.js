0850597fbf7b95f2b78e1e579a4c233268235a5e
starrpatty28 <starrpatty28@gmail.com>
2019-08-21 08:26:44 -0700

    Added a message for if you call an id thats not created and set a status at 400

--- IndependentDailyWork/ExpressJSPractice/index.js
+++ IndependentDailyWork/ExpressJSPractice/index.js
@@ -13,7 +13,15 @@ app.get("/api/members", (req, res) => res.json(members));
 
 //Get Single Member
 app.get("/api/members/:id", (req, res) => {
-  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if(found) {
+
+ res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else { 
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+  }
+
 });
 
 //Set static folder
