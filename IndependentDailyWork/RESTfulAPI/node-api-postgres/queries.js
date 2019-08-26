4560ce323323432ae07cd863065498c220e12cea
starrpatty28 <starrpatty28@gmail.com>
2019-08-25 23:57:00 -0700

    Created a /user/:id Request

--- IndependentDailyWork/RESTfulAPI/node-api-postgres/queries.js
+++ IndependentDailyWork/RESTfulAPI/node-api-postgres/queries.js
@@ -16,3 +16,14 @@ const getUsers = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
+
+//Request for user/:id 
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => { if (error) {
+        throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
