754bf6c0ca38d29ee9f60f439daad49cec8451ae
starrpatty28 <starrpatty28@gmail.com>
2019-08-25 23:00:45 -0700

    GET request to touch API Database, SELECT all users

--- IndependentDailyWork/RESTfulAPI/node-api-postgres/queries.js
+++ IndependentDailyWork/RESTfulAPI/node-api-postgres/queries.js
@@ -6,3 +6,13 @@ const pool = new Pool({
     password: 'password',
     port: 5432,
 })
+
+//GET request to touch API Database, SELECT all users
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if(error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
