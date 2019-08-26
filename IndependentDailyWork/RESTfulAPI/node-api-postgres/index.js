7a166b848c4fb4f670188cadea48bdd518274bf4
starrpatty28 <starrpatty28@gmail.com>
2019-08-25 21:46:15 -0700

    Initial commit, created an entry point for the server, require express and middleware and adding the port

---
+++ IndependentDailyWork/RESTfulAPI/node-api-postgres/index.js
@@ -0,0 +1,13 @@
+//The entry point for our server. We’ll require the express module, built in bodyParser middlware, and set our app and port variables
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
