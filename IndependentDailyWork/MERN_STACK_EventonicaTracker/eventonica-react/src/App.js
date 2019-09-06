824a5ff5149030a1d7c1e0d8f04b43fcce30259f
starrpatty28 <starrpatty28@gmail.com>
2019-09-05 19:16:11 -0700

    Created Components in src files, edited the index.html, added routes to app.js

--- IndependentDailyWork/MERN_STACK_EventonicaTracker/eventonica-react/src/App.js
+++ IndependentDailyWork/MERN_STACK_EventonicaTracker/eventonica-react/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component"
+import EventList from "./components/event-list.component";
+import EditEvent from "./components/edit-event.component";
+import CreateEvent from "./components/create-event.component";
+import CreateUser from "./components/create-user.component";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={EventList} />
+      <Route path="/edit/:id" component={EditEvent} />
+      <Route path="/create" component={CreateEvent} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
---
+++ IndependentDailyWork/MERN_STACK_EventonicaTracker/eventonica-react/src/components/create-event.component.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react'
+
+export default class CreateEvents extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Events component! </p>
+      </div>
+    )
+  }
+} 
+
