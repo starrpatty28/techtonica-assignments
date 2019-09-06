5fa4ff204d65ab735715b38603df3513cd37950d
starrpatty28 <starrpatty28@gmail.com>
2019-09-05 19:29:40 -0700

    Setting state for the create event page

--- IndependentDailyWork/MERN_STACK_EventonicaTracker/eventonica-react/src/components/create-event.component.js
+++ IndependentDailyWork/MERN_STACK_EventonicaTracker/eventonica-react/src/components/create-event.component.js
@@ -1,6 +1,29 @@
 import React, { Component} from 'react'
 
 export default class CreateEvents extends Component {
+  constructor(props) {
+    super(props);
+
+    //state is how you create variables in react
+    this.state = {
+      username: '',
+      event: '',
+      users: []
+    }
+  }
+
+onChangeUsername(e) {
+  this.setState({
+    username: e.target.value
+  });
+}
+
+onChangeEvent(e) {
+  this.setState({
+    event: e.target.value
+  });
+}
+
   render() {
     return (
       <div>
